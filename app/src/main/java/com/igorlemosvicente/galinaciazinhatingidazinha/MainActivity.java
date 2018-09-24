424856b37f878b4f2fc3f767af62275ab1c5de21
Igor L. V. - Laboratório 410 <igorlemosvicente@gmail.com>
2018-09-24 20:49:37 -0300

    Remove primeira etapa do curso, abandonando tudo por Recycler

--- app/src/main/java/com/igorlemosvicente/galinaciazinhatingidazinha/MainActivity.java
+++ app/src/main/java/com/igorlemosvicente/galinaciazinhatingidazinha/MainActivity.java
@@ -2,30 +2,12 @@ package com.igorlemosvicente.galinaciazinhatingidazinha;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    TextView textChick;
-    Button buttonSpawnChick;
-    int sumChicks = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // Referenciando
-        textChick = findViewById(R.id.textChick);
-        buttonSpawnChick = findViewById(R.id.buttonSpawnChick);
-
-        buttonSpawnChick.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                textChick.setText("Você já conseguiu " + (++sumChicks) + " pintos.");
-            }
-        });
     }
 }
